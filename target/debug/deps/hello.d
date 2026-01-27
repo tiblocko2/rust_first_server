cd05ef935c513a7a53dd2e29bf26ee791172843f
Dmitriy <team2tiblo@gmail.com>
2026-01-27 08:56:32 +0300

    html server

---
+++ target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\Admin\Documents\rust_server\hello\target\debug\deps\hello.d: src\main.rs
+
+C:\Users\Admin\Documents\rust_server\hello\target\debug\deps\hello.exe: src\main.rs
+
+src\main.rs:
---
+++ target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Users\Admin\Documents\rust_server\hello\target\debug\hello.exe: C:\Users\Admin\Documents\rust_server\hello\src\main.rs
